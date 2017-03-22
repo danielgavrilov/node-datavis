210fe99b88cc39884069eb58c91feb184aeafd81
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-03-22 20:25:58 +0000

    Actions modification

--- src/engine/actions.js
+++ src/engine/actions.js
@@ -1,8 +1,13 @@
+// cause recalc
 export const ADD_NODE = "ADD_NODE";
-export const MODIFY_NODE = "MODIFY_NODE"; // causes recalc
-export const MODIFY_NODE_PROPERTY = "MODIFY_NODE_PROPERTY"; // doesn't cause recalc
+export const MODIFY_NODE = "MODIFY_NODE";
 export const REMOVE_NODE = "REMOVE_NODE";
 export const CONNECT = "CONNECT";
 export const DISCONNECT = "DISCONNECT";
+
+// do not cause recalc
+export const MOVE_NODE = "MOVE_NODE";
+export const MOVE_NODE_END = "MOVE_NODE_END";
+
 // TODO when renaming a variable, rename 'variables' object key and also
 // rename 'variable' property of the node
