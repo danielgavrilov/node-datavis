381ee4d2d5492ef513f79d21c34ba5f4494ae5f6
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-03-29 16:55:03 +0100

    Reduce nesting

--- src/views/functions/FunctionsList.js
+++ src/views/functions/FunctionsList.js
@@ -3,11 +3,9 @@ import React from "react";
 import Function from "./Function";
 
 const FunctionsList = ({ functions, onFunctionClick }) => {
-  const children = functions.map((fn, id) => {
-    return (
-      <Function key={id} name={id} onClick={() => onFunctionClick(id)} />
-    );
-  }).toList().toJS();
+  const children = functions.map((fn, id) => (
+    <Function key={id} name={id} onClick={() => onFunctionClick(id)} />
+  )).toList().toJS();
   return (
     <div className="functions-list">
       {children.length > 0 ? children : <p>No functions found.</p>}
