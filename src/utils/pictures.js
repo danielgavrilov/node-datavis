42e37615a1e969313aed7aaa5daffa5e62a4119e
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-04-04 17:44:22 +0100

    Add getPicture util function

--- src/utils/pictures.js
+++ src/utils/pictures.js
@@ -1,5 +1,20 @@
+import { has } from "lodash";
+
+import { InexistingPicture } from "../engine/errors";
+import primitives from "../pictures/primitives";
+
 export function currentPicture(state) {
   const pictureId = state.getIn(["editor", "pictureId"]);
   return state.getIn(["pictures", pictureId]);
 }
 
+export function getPicture(state, pictureId) {
+  const pictures = state.get("pictures");
+  if (pictures.has(pictureId)) {
+    return pictures.get(pictureId);
+  } else if (has(primitives, pictureId)) {
+    return primitives[pictureId].picture;
+  } else {
+    throw new InexistingPicture({ pictureId });
+  }
+}
