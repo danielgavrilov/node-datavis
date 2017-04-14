c44b159436edf496a6b58fda4d9f4e5237c038da
Daniel Gavrilov <dgavrilov95@gmail.com>
2017-04-14 17:23:14 +0100

    Add empty primitives palette to canvas

--- src/views/canvas/CanvasPane.js
+++ src/views/canvas/CanvasPane.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Canvas from "./Canvas";
+import PrimitivesPalette from "./PrimitivesPalette";
 
 import "../../css/canvas-pane.css";
 
 const CanvasPane = () => (
   <div className="canvas-pane">
+    <PrimitivesPalette />
     <Canvas />
   </div>
 );
---
+++ src/views/canvas/PrimitivesPalette.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PrimitivesPalette = () => (
+  <div className="primitives-palette"></div>
+);
+
+export default PrimitivesPalette;
